b9656e4c488e45a4e3fa6492b8ef4d62a47ef299
Stefano <stefano@gruppobpc.com>
2014-12-31 16:05:27 +0100

    add scripts

--- install.php
+++ install.php
@@ -5,16 +5,17 @@ $fixer = $baseDir = __DIR__.'/php-cs-fixer.pre-commit.php';
 $git = getcwd().'/.git/hooks';
 $precommit = $git.'/pre-commit';
 
-if (!file_exists($git))
-{
+if (!file_exists($git)) {
     passthru('echo Git is not initialized'.$redir);
     die;
 }
 
-if (file_exists($precommit))
-{
+if (file_exists($precommit)) {
     // pre commit already in place, skipping
     die;
 }
 
+passthru('echo '.$redir);
+passthru('echo Installing PSR-Police pre-commit hook'.$redir);
+passthru('echo '.$redir);
 passthru('ln -s '.escapeshellarg($fixer).' '.escapeshellarg($precommit).$redir);
